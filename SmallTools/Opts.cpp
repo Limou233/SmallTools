3cfd5f4b52ad846998c4f83c6c9d9858f54a0066
昀轩 李 <forget20120218@163.com>
2025-02-10 17:30:14 +0800

    修改注释

--- SmallTools/Opts.cpp
+++ SmallTools/Opts.cpp
@@ -5,8 +5,9 @@ inline void pause(string __prompt = "按任意键继续....") {
 	cout << __prompt << endl;
 	getchar();//按任意键退出
 }
+
 namespace opt {
-	// 函数名前加inline是为了解决LNK200和1669错误
+	// 函数名前加inline是为了解决LNK200和LNK1669错误
 	 inline void win10_del_Darrows() {
 		system("reg add \"\
 			HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Shell Icons\" \
